b7423994e671269f8eafbf2039b4d31e93899b1a
Sahid jimenez <sahid_jimenez95@hotmail.com>
2019-03-21 10:25:14 -0600

    c

---
+++ src/app/interfaces/datos.interface.ts
@@ -0,0 +1,11 @@
+
+
+export interface trabajos{
+
+  nombre:string;
+  descripcion_trabajo:string;
+  localizacion?:number;
+  fecha?:number;
+  uid?:string;
+
+}
--- src/app/services/datos.service.ts
+++ src/app/services/datos.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-
+import { trabajos } from "../interfaces/datos.interface";
 @Injectable({
   providedIn: 'root'
 })
 export class DatosService {
 
-  private itemsCollection: AngularFirestoreCollection<any>;
+  private itemsCollection: AngularFirestoreCollection<trabajos>;
 
 
-  public datos :any[] = [];
+  public datos :trabajos[] = [];
 
   constructor(private afs: AngularFirestore){ }
 
   cargarDatos(){
 
-    this.itemsCollection = this.afs.collection<any>('trabajos');
+    this.itemsCollection = this.afs.collection<trabajos>('trabajos');
     return this.itemsCollection.valueChanges();
 
   }
